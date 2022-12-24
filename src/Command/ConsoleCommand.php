dc084ccc40314621dd27482b3c9975e6f8c277e8
Mark Story <mark@mark-story.com>
2022-12-23 22:42:25 -0500

    Align return type

--- src/Command/ConsoleCommand.php
+++ src/Command/ConsoleCommand.php
@@ -34,6 +34,7 @@ class ConsoleCommand extends Command
      * @param \Cake\Console\Arguments $args The command arguments.
      * @param \Cake\Console\ConsoleIo $io The console io
      * @return int|null|void The exit code or null for success
+     * @phpcsSuppress SlevomatCodingStandard.TypeHints.ReturnTypeHint.MissingNativeTypeHint
      */
     public function execute(Arguments $args, ConsoleIo $io)
     {
